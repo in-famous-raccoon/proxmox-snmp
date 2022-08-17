18c6f057805f0afd9b58ba21b1a7ed351f10b817
Prins Geryha <isdahl@haugnett.no>
2022-08-17 19:38:25 +0200

    Permission fix and SMART changed method.
    Switched from root folder to opt because of permission issues with root. Fixed SMART status by switching from tail to grep method.

--- snmp-smart-status.sh
+++ snmp-smart-status.sh
@@ -3,6 +3,6 @@ if [ "$1" = "-g" ]
 then
 echo .1.3.6.1.2.1.25.1.10
 echo STRING
-smartctl -H /dev/sda | tail -2 | nawk '{print $6}'
+smartctl -H /dev/sda | grep "PASSED" | nawk '{print $6}'
 fi
 exit 0
