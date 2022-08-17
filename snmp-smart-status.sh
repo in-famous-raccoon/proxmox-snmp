761000c44101826e1ead834c23bccda6d773232a
Prins Geryha <isdahl@haugnett.no>
2022-08-17 20:50:23 +0200

    Added NVME SMART Status support
    NVME disks are located elsewhere. Added command for nvme.

--- snmp-smart-status.sh
+++ snmp-smart-status.sh
@@ -3,6 +3,7 @@ if [ "$1" = "-g" ]
 then
 echo .1.3.6.1.2.1.25.1.10
 echo STRING
-smartctl -H /dev/sda | grep "PASSED" | nawk '{print $6}'
+if 
+smartctl -H /dev/sda | grep "PASSED" | nawk '{print $6}' && smartctl -H /dev/nvme0n1p3 | grep "PASSED" | nawk '{print $6}'
 fi
 exit 0
