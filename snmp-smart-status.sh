550011c21b4b850a94c49f682281d3304272c3bc
Cologran <72316053+Cologran@users.noreply.github.com>
2022-10-03 17:25:17 +0200

    Update snmp-smart-status.sh
    one "if" too much
    delete nvme -> for usual SATA disks

--- snmp-smart-status.sh
+++ snmp-smart-status.sh
@@ -3,7 +3,6 @@ if [ "$1" = "-g" ]
 then
 echo .1.3.6.1.2.1.25.1.10
 echo STRING
-if 
-smartctl -H /dev/sda | grep "PASSED" | nawk '{print $6}' && smartctl -H /dev/nvme0n1p3 | grep "PASSED" | nawk '{print $6}'
+smartctl -H /dev/sda | grep "PASSED" | nawk '{print $6}'
 fi
 exit 0
