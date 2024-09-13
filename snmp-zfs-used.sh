188ec5b69f52ee0bca13a6a741d818ea5bb50010
Jan Zahradník <mashbrno@users.noreply.github.com>
2024-09-13 19:45:20 +0200

    ZFS % usage

--- snmp-zfs-used.sh
+++ snmp-zfs-used.sh
@@ -3,6 +3,6 @@ if [ "$1" = "-g" ]
 then
 echo .1.3.6.1.2.1.25.1.11
 echo gauge
-zpool list rpool | grep "rpool" | awk '{print $3}' | sed 's/[A-Z]//g'
+zpool list rpool | grep "rpool" | awk '{print $8}' | sed 's/%//g'
 fi
 exit 0
