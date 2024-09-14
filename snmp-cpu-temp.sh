c415fe93535db25a9de1ef1381a2d4bf6b76c03b
Jan Zahradník <mashbrno@users.noreply.github.com>
2024-09-14 19:07:50 +0200

    Update snmp-cpu-temp.sh
    Reading CPU temperature using sensors package

--- snmp-cpu-temp.sh
+++ snmp-cpu-temp.sh
@@ -3,6 +3,6 @@ if [ "$1" = "-g" ]
 then
 echo .1.3.6.1.2.1.25.1.8
 echo gauge
-echo `cat /sys/class/thermal/thermal_zone2/temp | sed 's/\(.\)..$/.\1/'`
+echo `sensors -u | grep temp1_input -m 1 | awk '{print $2}' | sed 's/[0-9][0-9]$//'`
 fi
 exit 0
